246a0c170a3da54a0feea47aeb210515ff5324e1
AbdulQadirJillani <abdulqadir786687786@gmail.com>
2024-03-08 00:50:10 +0500

    amboss test version

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,32 @@ import Home from './components/Home/Home.jsx'
 import FirstYear from './components/Pages/FirstYear.jsx'
 import SecondYear from './components/Pages/SecondYear.jsx'
 import ThirdYear from './components/Pages/ThirdYear.jsx'
+import Amboss from './components/Pages/Amboss.jsx'
 import Loading from './components/Loading/Loading.jsx'
 import QA from './components/QA/QA.jsx'
+import Amb from './components/QA/Amb.jsx'
 
 export default function App() {
   const [arr, setArr] = useState(null)
   const [sem, setSem] = useState(null)
   const [name, setName] = useState(null)
   const [year, setYear] = useState(null)
+  const [amb, setAmb] = useState(null)
 
-  function update (s, n, y) {
+  function update (s, n, y, a) {
     setSem(s)
     setName(n)
     setYear(y)
+    setAmb(a)
   }
 
   if (sem && name && year) {
     const data = import(`./components/PastPapers/${sem}/${name}/${name}-${year}.jsx`)
+    data.then((mod) => setArr(mod.default))
+  }
+
+  if (amb) {
+    const data = import(`./components/Amboss/${amb}.jsx`)
     data.then((mod) => setArr(mod.default))
   }
 
@@ -33,7 +42,9 @@ export default function App() {
         <Route path='1st-year' element={<FirstYear update={update} />}/>
         <Route path='2nd-year' element={<SecondYear update={update} />}/>
         <Route path='3rd-year' element={<ThirdYear update={update} />}/>
+        <Route path='amboss' element={<Amboss update={update} />}/>
         <Route path={`${sem}/${name}/${year}`} element={arr && arr[0].info === `${sem}/${name}/${year}` ? <QA data={arr}/> : <Loading/>}/>
+        <Route path={`amboss/${amb}`} element={arr && arr[0].info === amb ? <Amb data={arr}/> : <Loading/>}/>
         <Route path='*' element={<Home />}/>
       </Route>
     </Routes>
---
+++ src/components/QA/Amb.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import styles from './QA.module.css'
+import { Link } from 'react-router-dom'
+
+export default function Amb(props) {
+    const qaArray = props.data
+    const [index, setIndex] = useState(0)
+    const [optionIndex, setOptionIndex] = useState(null)
+    const [ansIndex, setAnsIndex] = useState(null)
+    const [score, setScore] = useState(0)
+    const [lock, setLock] = useState(false)
+    const [lockcounter, setLockCounter] = useState(0)
+    const currentQA = qaArray[index]
+
+    function decrement() {
+        setIndex((prevIndex) => prevIndex - 1)
+        setOptionIndex(null)
+        setAnsIndex(null)
+        setLock(true)
+        setLockCounter(prevLockCounter => prevLockCounter - 1)
+    }
+
+    function increment() {
+        setIndex((prevIndex) => prevIndex + 1)
+        setOptionIndex(null)
+        setAnsIndex(null)
+        if (lockcounter < 0) setLockCounter(prevLockCounter => prevLockCounter + 1)
+        if (lockcounter === 0) setLock(false)
+        else setLock(true)
+    }
+
+    function checker(index, opt) {
+        setOptionIndex(index)
+        if (opt && !lock) {
+            setScore(prevScore => prevScore + 1)
+            setLock(true)
+        }
+    }
+
+    function show() {
+        for (let i=0; i<currentQA.answers.length; i++) {
+            if (currentQA.answers[i].bool) {
+                setAnsIndex(i)
+            }
+        }
+    }
+
+  return (
+    <>
+    <div className={styles.qawrapper}>
+
+        <header className={styles.qainfo}>
+            <p>
+                {currentQA.info}
+            </p>
+
+            <p>
+                {`${currentQA.id+1} out of ${qaArray.length}`}
+            </p>
+        </header>
+
+        <hr />
+
+        <main>
+            <p 
+            className={styles.question}>
+                {currentQA.question}
+            </p>
+
+            <ol>
+                {
+                currentQA.answers.map((opt, index) => 
+                <li
+                key={index} 
+                className={(index===optionIndex || index===ansIndex) ? 
+                    ((opt.bool) ? styles.optionCorrect : styles.optionWrong) 
+                    : styles.option}
+                onClick={() => checker(index, opt.bool)}>
+                    {opt.option}
+                    <p className={(index===optionIndex || index===ansIndex) ?
+                    styles.explanationshow : styles.explanationhide}>
+                        {opt.explanation}
+                    </p>
+                </li>)
+                }
+            </ol>
+            
+            <button 
+            onClick={show} 
+            className={styles.showbtn}>
+                Show Answer
+            </button>
+        </main>
+
+        <footer className={styles.btnlayout}>
+            <button 
+            onClick={decrement} 
+            disabled={currentQA.id===0} 
+            className={currentQA.id===0 ? styles.disabledbtn : styles.btn}>
+                Back
+            </button>
+
+            {
+            (currentQA.id + 1 === qaArray.length) ? 
+            <button 
+            onClick={() => document.querySelector('dialog').showModal()} 
+            className={styles.btn}>
+                Finish
+            </button> 
+            : 
+            <button 
+            onClick={increment} 
+            className={styles.btn}>
+                Next
+            </button>
+            }
+        </footer>
+        
+    </div>
+
+    <dialog>
+        <p>
+            Score: {score} / {qaArray.length}
+        </p>
+
+        <button
+        onClick={() => document.querySelector('dialog').close()}>
+            <Link className={styles.homebtn} to='/'>Home</Link>
+        </button>
+    </dialog>
+    </>
+  )
+}
